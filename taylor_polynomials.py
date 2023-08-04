50ca9a2cffee11092ccf1aebeac8a34bfdab50dd
Tiendat1712 <tiendat.phan0101@gmail.com>
2023-08-05 00:57:33 +0200

    adding img and file csv

--- taylor_polynomials.py
+++ taylor_polynomials.py
@@ -3,6 +3,7 @@ import numpy as np
 import sympy as sp 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import pandas as pd
 
 
 t, a = sp.symbols('t a')
@@ -37,7 +38,34 @@ def taylor_terms(func, order, point, derivatives = None):
 
     return taylor_terms(derivative, order, point, derivatives)
 
+def taylor_polynomials(terms):
+    '''
+    find Taylor polynomials
+    func: symbolic function to approximate
+    order: highest order derivative for Taylor polynomial
+    point: point about which the function is approximated
+    derivatives: list of Taylor terms
+    '''
 
+    # initialize list
+    polynomials = []
+
+    # initialize taylor polynomial
+    poly = None
+
+    for term in range(len(terms)):
+        # build up polynomial on each iteration
+        poly = terms[term] if poly is None else poly + terms[term]
+
+        polynomials.append(poly)
+
+    return polynomials
+
+def reg(x):
+    if x == 1:
+        return x
+    
+    return x*reg(x-1)
 if __name__ == '__main__':
 
     # analysis label
@@ -57,6 +85,34 @@ if __name__ == '__main__':
     time = np.arange(start, stop, 0.1)
 
     # find talor polynomial term describing function f(t)
-    symbolic_derivates, term = taylor_terms(func=f,order= 4, point = approximation_point)
+    symbolic_derivates, terms = taylor_terms(func=f,order= 4, point = approximation_point)
+    polys = taylor_polynomials(terms)
 
-    print(term) 
+    print(polys)
+   
+   # initialize plot
+    fig, ax = plt.subplots()
+    ax.set(xlabel = 't', ylabel ='y', title = 'Taylor Polynomial Approximation: {label}')
+    legend = []
+
+    for p, poly in enumerate(polys):
+        ax.plot(time, [poly.subs({t: point}) for point in time])
+        legend.append(f'P{p}')
+
+    ax.plot(time, [f.subs({t: point}) for point in time])
+    legend.append(f'f(t)')
+
+    # create a data frame
+
+    df = pd.DataFrame({'symbolic_derivates': symbolic_derivates,
+                       'taylor_term': terms,
+                       'polynomials': polys})
+    
+    # save and show the results
+
+    ax.legend(legend)
+    ax.grid()
+    plt.savefig(f'taylor_{label}.png')
+    plt.show()
+    df.to_csv(f'taylor_{label}.csv', encoding ='utf-8')
+    print(df.head(10))
